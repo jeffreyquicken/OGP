43f5284108c8142ad115fdc2ed68f8b57ab2cac7
jeffreyquicken <jeffreyquicken@Jeffreys-MacBook-Air.local>
2017-03-09 14:52:31 +0100

    Added test methods
    Added getTimeToCollision test methods

--- OGP1617-Asteroids/tests/asteroids/tests/ShipTests.java
+++ OGP1617-Asteroids/tests/asteroids/tests/ShipTests.java
@@ -247,7 +247,32 @@ public class ShipTests {
 	
 	@Test 
 	public void getTimeToCollision_ScalarProdGreaterthan0(){
-		
+		distanceShip1.setVelY(10);
+		distanceShip2.setVelY(10);
+		assertEquals(Double.POSITIVE_INFINITY, distanceShip1.getTimeToCollision(distanceShip2), EPSILON);
+	}
+	
+	@Test
+	public void getTimeToCollision_LegalCase(){
+		distanceShip1.setPosY(0);
+		distanceShip2.setPosY(0);
+		distanceShip1.setVelY(0);
+		distanceShip2.setVelY(0);
+		distanceShip1.setVelX(5);
+		assertEquals(1, distanceShip1.getTimeToCollision(distanceShip2), EPSILON);
+	}
+	
+	@Test
+	public void getTimeToCollision_dSmallerThen0(){
+		distanceShip1.setVelX(10);
+		distanceShip1.setVelY(10);
+		distanceShip2.setVelX(0);
+		distanceShip2.setVelY(20);
+		distanceShip1.setPosX(20);
+		distanceShip1.setPosY(101);
+		distanceShip2.setPosX(10);
+		distanceShip2.setPosY(1);
+		assertEquals(Double.POSITIVE_INFINITY, distanceShip1.getTimeToCollision(distanceShip2), EPSILON);
 	}
 	
 	@Test
@@ -260,5 +285,10 @@ public class ShipTests {
 	public void getTimeToCollision_IllegalArgumentCase(){
 		exception.expect(IllegalArgumentException.class);
 		collisionShip1.getTimeToCollision(collisionShip1);
+	}
+	
+	@Test
+	public void collisionPosition_LegalCas(){
+		
 	}
 }
